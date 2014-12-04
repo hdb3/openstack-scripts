46b45e99dfe034a7f5d2ffdcb19705f744069744
Nic Hart <ou@hdb3.net>
2014-12-04 19:46:12 +0000

    tidy

--- parse.py
+++ parse.py
@@ -3,7 +3,6 @@ from HTMLParser import HTMLParser
 
 import sys
 
-# create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print "Encountered a start tag:", tag
@@ -12,12 +11,7 @@ class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         print "Encountered some data  :", data
 
-#with open(sys.argv[1], 'r') as my_file:
-#    print(my_file.read())
-
 my_file = open(sys.argv[1], 'r')
-# instantiate the parser and fed it some HTML
+
 parser = MyHTMLParser()
 parser.feed(my_file.read())
-#parser.feed('<html><head><title>Test</title></head>'
-#            '<body><h1>Parse me!</h1></body></html>')
