9e2d6d0fb8e955d7c9e1bc6c6d74df382f53dcfc
Nic Hart <ou@hdb3.net>
2014-12-04 19:45:25 +0000

    converted to parse argv[1]

--- parse.py
+++ parse.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python2.7
 from HTMLParser import HTMLParser
+
+import sys
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
@@ -9,7 +12,12 @@ class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         print "Encountered some data  :", data
 
+#with open(sys.argv[1], 'r') as my_file:
+#    print(my_file.read())
+
+my_file = open(sys.argv[1], 'r')
 # instantiate the parser and fed it some HTML
 parser = MyHTMLParser()
-parser.feed('<html><head><title>Test</title></head>'
-            '<body><h1>Parse me!</h1></body></html>')
+parser.feed(my_file.read())
+#parser.feed('<html><head><title>Test</title></head>'
+#            '<body><h1>Parse me!</h1></body></html>')
