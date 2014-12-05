cd350cfb2d8a85f2dd0ff175df2b688f132f4124
Nic Hart <ou@hdb3.net>
2014-12-05 12:16:08 +0000

    more tags found, bugs fixed

--- parse.py
+++ parse.py
@@ -25,16 +25,18 @@ class MyHTMLParser(HTMLParser):
             return self.watches[tag,attr,val]
         if ("",attr,val) in self.watches:
             return self.watches["",attr,val]
+        if (tag,"","") in self.watches:
+            return self.watches[tag,"",""]
         return None
     def handle_starttag(self, tag, attrs):
         for (k,v) in attrs:
             if (self.check(tag,k,v)):
-                print "!Encountered a special tag:", tag,k,v
+                print "!Encountered a wanted tag:", tag,k,v
                 self.stack.append((tag,k,v,[]))
                 return # prevent pushing multiple items since we won't know how to remove them
         print "?Encountered an unwanted start tag:", tag # only gets here if no wanted tags/attributes are seen
     def handle_endtag(self, tag):
-        if (self.stack and self.stack[-1] == tag):
+        if (self.stack and self.stack[-1][0] == tag):
             t,k,v,d = self.stack.pop()
             print "!Encountered a stacked end tag :", tag,k,v, "with ", len(d), " data items"
         else:
@@ -50,5 +52,7 @@ my_file = open(sys.argv[1], 'r')
 
 parser = MyHTMLParser()
 parser.register("","class","screen","code")
+parser.register("","class", "programlisting brush: bash; " ,"code")
+parser.register("code","","","code")
 parser.feed(my_file.read())
 parser.close()
