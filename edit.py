549e2448f33b8c3d2ea8d121c6f8e0049fce0132
Nic Hart <ou@hdb3.net>
2014-12-08 16:48:00 +0000

    fix bug which deleted wrong lines and put items before section headers...

--- edit.py
+++ edit.py
@@ -111,12 +111,13 @@ class Editor:
                 with open(out_file_path,'w') as outfile:
                     mark=0
                     for section,name,line,d_ln,delete in edits:
-                        while (mark<=line):
+                        while (mark<line):
                             outfile.write(scripts.file[mark] + "\n")
                             mark += 1
+                        if (not delete):
+                            outfile.write(scripts.file[mark] + "\n")
+                        mark += 1
                         outfile.write(self.file[d_ln] + "\n")
-                        if (delete):
-                            mark += 1
                     while (mark<len(scripts.file)):
                         outfile.write(scripts.file[mark] + "\n")
                         mark += 1
