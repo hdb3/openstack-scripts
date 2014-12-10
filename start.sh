b4b9c14fe454d3fb3f19443614e83f56a7273546
Nic Hart <nic@dell2>
2014-12-10 11:39:43 +0000

    add a rabit command to start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@ sudo bash openstack.apt.sh
 tar zxf content.tgz &&
 ./build-script.sh &&
 ./files.py total.txt total.files &&
-./sql.sh &&
-./filter.sh total.txt > total.sh
+# ./sql.sh &&
+./filter.sh total.txt > total.sh &&
+./edit.py total.files  &
+sudo rabbitmqctl change_password guest admin
