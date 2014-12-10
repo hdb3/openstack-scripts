6c3bcccf18fc49ce377a9d30344a5d559d210cf6
Nic Hart <ou@hdb3.net>
2014-12-10 10:14:20 +0000

    add openstack-utils.sh openstack.sh

---
+++ openstack-utils.sh
@@ -0,0 +1,105 @@
+# keystone tenant-create --name admin --description "Admin Tenant"
+# TENANT admin "Admin Tenant"
+function COMMAND {
+ echo "$*"
+}
+
+function TENANT {
+ echo "keystone tenant-create --name $1 --description $2"
+}
+
+# keystone user-create --name admin --pass admin --email user@example.com
+# USER admin admin user@example.com
+function USER {
+ echo "keystone user-create --name $1 --pass $2"
+ # echo "keystone user-create --name $1 --pass $2 --email $3"
+}
+
+# keystone role-create --name admin
+# ROLE admin
+function ROLE {
+ echo "keystone role-create --name $1"
+}
+
+# keystone user-role-add --tenant admin --user admin --role admin
+# ROLE-ADD admin admin admin
+function ROLE-ADD {
+ echo "keystone user-role-add --tenant $1 --user $2 --role $3"
+}
+
+
+# keystone service-create --name keystone --type identity --description "OpenStack Identity"
+# SERVICE keystone identity "OpenStack Identity"
+function SERVICE {
+ echo "keystone service-create --name $1 --type $2 --description $3"
+}
+
+# keystone endpoint-create --service-id $(keystone service-list | awk '/ identity / {print $2}') --publicurl http://controller:5000/v2.0 --internalurl http://controller:5000/v2.0 --adminurl http://controller:35357/v2.0 --region regionOne
+# ENDPOINT regionOne identity 5000/v2.0 5000/v2.0 35357/v2.0
+function ENDPOINT {
+ADDR=controller
+if [[ $# > 4 ]] ; then
+ echo "keystone endpoint-create --service-id \$(keystone service-list | awk '/ $2 / {print \$2}') --publicurl http://${ADDR}:${3} --internalurl http://${ADDR}:${4} --adminurl http://${ADDR}:${5} --region $1"
+else
+ echo "keystone endpoint-create --service-id \$(keystone service-list | awk '/ $2 / {print \$2}') --publicurl http://${ADDR}:${3} --internalurl http://${ADDR}:${3} --adminurl http://${ADDR}:${3} --region $1"
+fi
+
+}
+
+# keystone --os-tenant-name admin --os-username admin --os-password admin --os-auth-url http://controller:35357/v2.0 token-get
+# KADMIN token-get
+function KADMIN {
+ echo "keystone --os-tenant-name admin --os-username admin --os-password admin --os-auth-url http://controller:35357/v2.0 $1"
+}
+
+# keystone --os-tenant-name demo --os-username demo --os-password admin --os-auth-url http://controller:35357/v2.0 token-get
+# KDEMO token-get
+function KDEMO {
+ echo "keystone --os-tenant-name demo --os-username demo --os-password admin --os-auth-url http://controller:35357/v2.0 $1"
+}
+
+# glance image-create --name "cirros-0.3.3-x86_64" --file cirros-0.3.3-x86_64-disk.img --disk-format qcow2 --container-format bare --is-public True --progress
+# IMAGE "cirros-0.3.3-x86_64" cirros-0.3.3-x86_64-disk.img qcow2 bare True
+function IMAGE {
+ echo "glance image-create --name $1 --file $2 --disk-format $3 --container-format $4 --is-public True --progress"
+}
+
+# glance image-list
+# IMAGE-LIST
+function IMAGE-LIST {
+ echo "glance image-list"
+}
+
+# neutron subnet-create ext-net --name ext-subnet --allocation-pool start=10.30.66.2,end=10.30.66.50 --disable-dhcp --gateway 10.30.66.1 10.30.66.0/24
+# SUBNET ext-net ext-subnet --allocation-pool start=10.30.66.2,end=10.30.66.50 --disable-dhcp --gateway 10.30.66.1 10.30.66.0/24
+function SUBNET {
+if [[ $# > 4 ]] ; then
+ echo "neutron subnet-create ext-net --name $1 --allocation-pool start=$4,end=$5 --disable-dhcp --gateway $2 $3"
+else
+ echo "neutron subnet-create ext-net --name $1 --disable-dhcp --gateway $2 $3"
+fi
+}
+
+# neutron net-create ext-net --shared --router:external True --provider:physical_network external --provider:network_type flat
+# NET ext-net
+function NET {
+ echo "neutron net-create $1 --shared --router:external True --provider:physical_network external --provider:network_type flat"
+}
+
+# neutron router-create demo-router
+# ROUTER demo-router
+function ROUTER {
+ echo "neutron router-create $1"
+}
+
+# neutron router-interface-add demo-router demo-subnet
+# INTERFACE demo-router demo-subnet
+function INTERFACE {
+ echo "neutron router-interface-add $1 $2"
+}
+
+# neutron router-gateway-set demo-router ext-net
+# GATEWAY demo-router ext-net
+function GATEWAY {
+ echo "neutron router-gateway-set-interface-add $1 $2"
+}
---
+++ openstack.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+source openstack-utils.sh
+
+	TENANT		admin	"Admin Tenant"
+	USER		admin	admin	user@example.com
+	ROLE		admin
+	ROLE-ADD	admin	admin	admin
+	ROLE		_member_
+	ROLE-ADD	admin	admin	_member_
+	TENANT		demo	"Demo	Tenant"
+	USER		demo	admin	user@example.com
+	ROLE-ADD	tenant	demo	demo	role	_member_
+	TENANT		service	"Service Tenant"
+
+	SERVICE		keystone	identity	"OpenStack Identity"
+	ENDPOINT	regionOne	identity	5000/v2.0	5000/v2.0	35357/v2.0
+
+#verification..
+	KADMIN		token-get
+	KADMIN		tenant-list
+	KADMIN		user-list
+	KADMIN		role-list
+
+#verification..
+
+	KDEMO		token-get
+	KDEMO		user-list
+	unset		OS_SERVICE_TOKEN	OS_SERVICE_ENDPOINT	regionOne
+	KADMIN		token-get
+	KADMIN		tenant-list
+	KADMIN		user-list
+	KADMIN		role-list
+	KDEMO		token-get
+	KDEMO		user-list
+	COMMAND		source		admin-openrc.sh
+
+
+
+	USER		glance	admin
+	ROLE-ADD	glance	service	admin
+	SERVICE		glance	--type	image	"OpenStack Image Service"
+	ENDPOINT	regionOne	image	9292
+	IMAGE		"cirros-0.3.3-x86_64"	cirros-0.3.3-x86_64-disk.img	qcow2	bare	True
+	IMAGE-LIST
+
+	USER		neutron	admin
+	ROLE-ADD	neutron	service	admin
+	SERVICE		neutron	--type	network	"OpenStack Networking"
+	ENDPOINT	regionOne	network	9696
+	COMMAND		tenant-get	service
+	COMMAND		neutron	ext-list
+	SUBNET		ext-net	ext-subnetg10.30.66.2	10.30.66.50	10.30.66.1	10.30.66.0/24
+	NET		ext-netg
+	SUBNET		ext-net	ext-subnet	10.30.66.2	10.30.66.50	10.30.66.1	10.30.66.0/24
+	NET		demo-net
+	SUBNET		demo-net	demo-subnet	192.168.1.1	192.168.1.0/24
+	ROUTER		demo-router
+	INTERFACE	demo-router	demo-subnet
+	GATEWAY		demo-router	ext-net
+	USER		cinder	admin
+	ROLE-ADD	cinder	service	admin
+	SERVICE		cinder	volume	"OpenStack Block Storage"
+	SERVICE		cinderv2	volumev2	"OpenStack	Block	Storage"
+	ENDPOINT	regionOne	volume	8776/v1/%\(tenant_id\)s
+	ENDPOINT	regionOne	volumev2	8776/v2/%\(tenant_id\)s
+	COMMAND		cinder		create	--display-name	demo-volume1	1
+	USER		heat	admin
+	ROLE-ADD	heat	service	admin
+	ROLE		heat_stack_user
+	ROLE		heat_stack_owner
+	SERVICE		heat	orchestration	"Orchestration"
+	SERVICE		heat-cfn	cloudformation	"Orchestration"
+	ENDPOINT	regionOne	orchestration	8004/v1/%\(tenant_id\)s
+	ENDPOINT	regionOne	cloudformation	8000/v1
+	COMMAND		"NET_ID=\$(neutron	net-list	|	awk	'/	demo-net	/	{	print	\$2	}')"
+	COMMAND		heat		stack-create	-f	test-stack.yml	-P	"ImageID=cirros-0.3.3-x86_64;NetID=$NET_ID"	testStack
+	USER		ceilometer	admin
+	ROLE-ADD	ceilometer	service	admin
+	SERVICE		ceilometer	metering	"Telemetry"
+	ENDPOINT	regionOne	metering	8777
