cf04efb6fb506e1ee82d580b66c4d2a8b2f426ab
Nic Hart <nic@dell2>
2014-12-11 14:17:34 +0000

    enable the neutron.conf script in start.sh

--- update-neutron-conf.sh
+++ update-neutron-conf.sh
@@ -5,4 +5,4 @@ echo "The ID is $SERVICE_TENANT_ID"
 echo "{/etc/neutron/neutron.conf}" > neutron.conf
 echo "[DEFAULT]" >> neutron.conf
 echo "nova_admin_tenant_id = $SERVICE_TENANT_ID" >> neutron.conf
-#sudo ./edit.py -v -w neutron.conf
+sudo ./edit.py -v -w neutron.conf
