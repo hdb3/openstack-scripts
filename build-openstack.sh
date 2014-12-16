6e9b612931814f410ba24417702d7e0285d8e0e2
Nic Hart <nic@dell2>
2014-12-16 11:29:09 +0000

    add the fedora image from devstack

--- build-openstack.sh
+++ build-openstack.sh
@@ -44,6 +44,8 @@ source openstack-utils.sh
 	ENDPOINT	regionOne	image	9292
 	COMMAND		"wget http://cdn.download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img"
 	IMAGE		"cirros-0.3.3-x86_64"	cirros-0.3.3-x86_64-disk.img	qcow2	bare	True
+	COMMAND         "wget https://download.fedoraproject.org/pub/alt/openstack/20/x86_64/Fedora-x86_64-20-20140618-sda.qcow2"
+	IMAGE		"Fedora-x86_64-20-20140618"	Fedora-x86_64-20-20140618-sda.qcow2    qcow2   bare    True
 	IMAGE-LIST
 
 	USER		nova	admin
