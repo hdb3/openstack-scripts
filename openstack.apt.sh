a91be9376fa4936aa13bb55f25f30dadfabcc206
Nic Hart <nic@x61>
2014-12-20 20:49:27 +0000

    reapply improvement in openstack.apt.sh

--- openstack.apt.sh
+++ openstack.apt.sh
@@ -1,4 +1,4 @@
 apt-get install ubuntu-cloud-keyring &&
 echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu" "trusty-updates/juno main" > /etc/apt/sources.list.d/cloudarchive-juno.list &&
-apt-get update &&  apt-get -y dist-upgrade &&
-for f in `cat openstack.packages` ; do apt-get install -y $f ; done
+for f in `cat openstack.packages` ; do fs="$fs $f" ; done
+apt-get update &&  apt-get -y dist-upgrade && apt-get -y install $fs
