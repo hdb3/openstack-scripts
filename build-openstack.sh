ac4f140dcc0821d0f06e0c7e4aac85bbf6aace6d
Nic Hart <ou@hdb3.net>
2014-12-21 15:54:32 +0000

    add back the demo network definition

--- build-openstack.sh
+++ build-openstack.sh
@@ -59,14 +59,14 @@ source openstack-utils.sh
 	ENDPOINT	regionOne	network	9696
 	COMMAND		"keystone tenant-get	service"
 	COMMAND		"neutron	ext-list"
-#	EXTNET		ext-net
+	EXTNET		ext-net
 	#		network name	subnet name	gateway		CIDR		alloc start	alloc end
-#	SUBNET	ext-net		ext-subnet	10.30.65.1      10.30.65.0/24	10.30.65.2	10.30.65.50
-#	NET		demo-net
-#	SUBNET		demo-net	demo-subnet	192.168.1.1	192.168.1.0/24
-#	ROUTER		demo-router
-#	INTERFACE	demo-router	demo-subnet
-#	GATEWAY		demo-router	ext-net
+	SUBNET	ext-net		ext-subnet	10.30.65.1      10.30.65.0/24	10.30.65.2	10.30.65.50
+	NET		demo-net
+	SUBNET		demo-net	demo-subnet	192.168.1.1	192.168.1.0/24
+	ROUTER		demo-router
+	INTERFACE	demo-router	demo-subnet
+	GATEWAY		demo-router	ext-net
 	USER		cinder	admin
 	ROLE-ADD	cinder	service	admin
 	SERVICE		cinder	volume	"OpenStack Block Storage"
