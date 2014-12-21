a0bf6f4d1678119cb20d8c290ebb3d11a90f4770
Nic Hart <nic@x61>
2014-12-21 11:12:00 +0000

    force package install to run again

--- openstack.apt.sh
+++ openstack.apt.sh
@@ -1,4 +1,4 @@
 apt-get install ubuntu-cloud-keyring &&
 echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu" "trusty-updates/juno main" > /etc/apt/sources.list.d/cloudarchive-juno.list &&
 for f in `cat openstack.packages` ; do fs="$fs $f" ; done
-apt-get update &&  apt-get -y dist-upgrade && apt-get -y install $fs
+apt-get update &&  apt-get -y dist-upgrade && apt-get -y install --reinstall $fs
