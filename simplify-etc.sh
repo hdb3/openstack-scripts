597a231e286e0ec3ebe1e79e6c149cc546528c42
Nic Hart <ou@hdb3.net>
2014-12-24 12:34:25 +0000

    simplify-etc.sh

---
+++ simplify-etc.sh
@@ -0,0 +1,4 @@
+for f in `cat /home/nic/openstack-scripts/projects` ; do ff="$ff /etc/$f" ; done
+find $ff -type f \( -name \*conf -or -name \*ini \) -exec /home/nic/openstack-scripts/simplify.py -i "{}" \; &&
+rm -rf etc.tmp && mkdir -p etc.tmp &&
+find $ff -type f \( -name \*conf -or -name \*ini \) -exec cp "{}" etc.tmp \;
