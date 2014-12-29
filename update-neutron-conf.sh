93be3648ee188bf0abef5c4a977c7d21d9c6520f
Nic Hart <ou@hdb3.net>
2014-12-29 17:30:16 +0000

    modernise update-neutron-conf.sh

--- update-neutron-conf.sh
+++ update-neutron-conf.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
+conf_file=/etc/neutron/neutron.conf
 source admin-openrc.sh
 SERVICE_TENANT_ID=`keystone tenant-list | awk '/ service / {print $2}'`
-echo "The ID is $SERVICE_TENANT_ID"
-echo "{/etc/neutron/neutron.conf}" > neutron.conf
-echo "[DEFAULT]" >> neutron.conf
-echo "nova_admin_tenant_id = $SERVICE_TENANT_ID" >> neutron.conf
-sudo ./edit.py -v -w neutron.conf
+echo "The SERVICE_TENANT_ID is $SERVICE_TENANT_ID"
+echo "updating $conf_file"
+sudo sed -i.bak -e "s/\$SERVICE_TENANT_ID/$SERVICE_TENANT_ID/" $conf_file
+sudo chown --reference ${conf_file}.bak $conf_file
