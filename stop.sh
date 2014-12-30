8c43c04cfc82fa4966d4702db5dffb2d83315238
Nic Hart <ou@hdb3.net>
2014-12-30 11:54:40 +0000

    improve stop.sh

--- stop.sh
+++ stop.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
 for s in `cat services`
     do
         echo "stop $s"
         sudo service $s stop
-        sudo rm -f /var/log/$s
     done 
 
 for project in `cat projects`
     do
         echo "clean log files for $project"
-        sudo truncate -s 0 /var/log/$project/*
+        sudo rm -f /var/log/$project
     done
