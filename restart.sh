f85667842cae18e80197cdc4e1070e46f6fe08f8
Nic Hart <ou@hdb3.net>
2014-12-30 15:19:12 +0000

    clean up restart.sh

--- restart.sh
+++ restart.sh
@@ -1,15 +1,14 @@
-
+#!/bin/bash
 for s in `cat services`
     do
         echo "stop $s"
         sudo service $s stop
-        sudo rm -f /var/log/$s
     done 
 
 for project in `cat projects`
     do
         echo "clean log files for $project"
-        sudo truncate -s 0 /var/log/$project
+        sudo rm -rf /var/log/$project/*
     done
 
 for s in `cat services`
