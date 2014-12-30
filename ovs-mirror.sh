470d13672b6b9eb459b3ed020167b492bec9b63b
Nic Hart <ou@hdb3.net>
2014-12-30 09:20:57 +0000

    switch to use dummy device for mirroring

--- ovs-mirror.sh
+++ ovs-mirror.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -ve
+#!/bin/bash -e
 
-sudo ip link add veth0 type veth peer name veth1
-sudo ip link set link dev veth0 up
-sudo ip link set link dev veth1 up
-sudo ovs-vsctl add-port $1 veth0 \
-           -- --id=@p get port veth0 \
+sudo ip link add dummy0 type dummy
+sudo ip link set link dev dummy0 up
+sudo ovs-vsctl add-port $1 dummy0 \
+           -- --id=@p get port dummy0 \
 	   -- --id=@m create mirror name=m0 select-all=true select-vlan=$2 output-port=@p \
 	   -- set bridge $1 mirrors=@m
+echo "success - you may now monitor port 'dummy0' to see traffic from bridge '$1'/ vlan '$2'"
+echo "clear this mirror using the scrip ovs-mirror-off.sh or the command 'ovs-vsctl clear bridge $1 mirrors'"
