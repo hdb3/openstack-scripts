29693275c6f7c5835a5bdee4f2593190c906f7e0
Nic Hart <ou@hdb3.net>
2014-12-31 18:55:25 +0000

    adapted the service restart scripts to support yum and deb forms, including that neutron service are not prefixed with openstack- unlike the others

--- build-db_sync.sh
+++ build-db_sync.sh
@@ -8,38 +8,32 @@ DBPASS=root
 # KEYSTONE
  DB keystone keystone admin
  SU keystone "keystone-manage db_sync"
- RESTART keystone
- RM /var/lib/keystone/keystone.db
+ RESTART KEYSTONE
 
 # GLANCE
  DB glance glance admin
  SU glance "glance-manage db_sync"
- RESTART glance-registry glance-api
+ RESTART GLANCE
 
 # NOVA
  DB nova nova admin
  SU nova "nova-manage db sync"
- RESTART nova-api nova-cert nova-consoleauth nova-scheduler nova-conductor nova-novncproxy
+ RESTART NOVA
 
 # NEUTRON
  DB neutron neutron admin
  SU neutron "neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade juno"
- RESTART nova-api nova-scheduler nova-conductor neutron-server nova-api neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent nova-compute neutron-plugin-openvswitch-agent
+ RESTART NOVA
+ RESTART NEUTRON
 
 # CINDER
  DB cinder cinder admin
  SU cinder "cinder-manage db sync"
- RESTART cinder-scheduler cinder-api tgt cinder-volume
- RM /var/lib/cinder/cinder.sqlite
+ RESTART CINDER
 
 # HEAT
  DB heat heat admin
  SU heat "heat-manage db_sync"
- RESTART heat-api heat-api-cfn heat-engine
- RM /var/lib/heat/heat.sqlite
+ RESTART HEAT
 
  END
-
-
-
-
--- utils.sh
+++ utils.sh
@@ -1,3 +1,5 @@
+#service lists...
+
 function END {
 echo "echo 'Finished...'"
 }
@@ -10,13 +12,6 @@ function SU {
  echo "sudo su -s /bin/sh -c '$2' $1"
 }
 
-function RESTART {
-for arg
-do
-    echo "sudo service ${SERVICE_PREFIX}$arg restart"
-done
-}
-
 function MYSQL {
     echo "mysql -u $DBUSER --password=$DBPASS -vv -f -e \"$1\""
 }
@@ -26,4 +21,24 @@ MYSQL "DROP DATABASE IF EXISTS $1;"
 MYSQL "CREATE DATABASE $1;"
 MYSQL "GRANT ALL PRIVILEGES ON $1.* TO '$2'@'localhost'   IDENTIFIED BY '$3';"
 MYSQL "GRANT ALL PRIVILEGES ON $1.* TO '$2'@'%'   IDENTIFIED BY '$3';"
+}
+
+function _RESTART {
+PREFIX=$1
+shift
+for arg
+do
+    echo "sudo service ${PREFIX}$arg restart"
+done
+}
+
+function RESTART {
+case $1 in
+KEYSTONE) _RESTART ${SERVICE_PREFIX} keystone ;;
+GLANCE) _RESTART ${SERVICE_PREFIX} glance-registry glance-api ;;
+NOVA) _RESTART ${SERVICE_PREFIX} nova-api nova-cert nova-consoleauth nova-scheduler nova-conductor nova-novncproxy ;;
+NEUTRON) _RESTART "" neutron-server nova-api neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent nova-compute neutron-plugin-openvswitch-agent ;;
+CINDER) _RESTART ${SERVICE_PREFIX} cinder-scheduler cinder-api tgt cinder-volume ;;
+HEAT) _RESTART ${SERVICE_PREFIX} heat-api heat-api-cfn heat-engine ;;
+esac
 }
