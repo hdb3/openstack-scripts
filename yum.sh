5e614154e28000621c805196a184848c68adcd38
Nic Hart <ou@hdb3.net>
2014-12-31 07:35:21 +0000

    add -y to yum

--- yum.sh
+++ yum.sh
@@ -1,5 +1,5 @@
-sudo yum install yum-plugin-priorities
-sudo yum install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-sudo yum install http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
-sudo yum upgrade
-sudo yum install `cat yum.list`
+yum install -y yum-plugin-priorities
+yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum install -y http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
+yum upgrade -y
+yum install -y `cat yum.list`
