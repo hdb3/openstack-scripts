abe294ada0782c1032ccf6d43f6ac26c4bba1656
Nic Hart <ou@hdb3.net>
2015-01-01 17:51:19 +0000

    fixing new ./build-openstack.sh

--- build-openstack.sh
+++ build-openstack.sh
@@ -2,8 +2,9 @@
 source openstack-utils.sh
 set -e
 
-	ADMINRC
+	TOKEN_UNAUTH
 	KADMIN		token-get
+	ADMINRC
 	#COMMAND		"wget http://cdn.download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img"
 	#IMAGE		"cirros-0.3.3-x86_64"	cirros-0.3.3-x86_64-disk.img	qcow2	bare	True
 	IMAGE		"Fedora-x86_64-20-20140618"	../Fedora-x86_64-20-20140618-sda.qcow2	qcow2	bare	True
