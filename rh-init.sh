09ffc5ed7d22919da308ef81ff7402f9cf1a9f63
Nic Hart <ou@hdb3.net>
2015-01-02 10:11:27 +0000

    rollup all changes

--- rh-init.sh
+++ rh-init.sh
@@ -4,7 +4,7 @@ yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.
 yum install -y http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
 yum upgrade -y
 yum install -y `cat yum.list`
-sudo systemctl restart rabbitmq-server
-read -t 10 -i "pausing to allow the rabbit to settle"
 sudo rabbitmqctl change_password guest admin
 sudo ln -s /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugin.ini
+sudo sed -i 's,plugins/openvswitch/ovs_neutron_plugin.ini,plugin.ini,g' /usr/lib/systemd/system/neutron-openvswitch-agent.service
+sudo chown -R apache:apache /usr/share/openstack-dashboard/static
