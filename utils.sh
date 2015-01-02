963886536152b2c254d4b37511225c2129e03189
Nic Hart <ou@hdb3.net>
2015-01-02 12:43:19 +0000

    remove the neutron-openvswitch-agent like entry from common list

--- utils.sh
+++ utils.sh
@@ -71,7 +71,7 @@ function _RESTART {
 KEYSTONE_SERVICES="keystone"
 GLANCE_SERVICES="glance-registry glance-api"
 NOVA_SERVICES="nova-api nova-cert nova-consoleauth nova-scheduler nova-conductor nova-novncproxy nova-compute"
-NEUTRON_SERVICES="neutron-server neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent "
+NEUTRON_SERVICES="neutron-server neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent "
 CINDER_SERVICES="cinder-scheduler cinder-api tgt cinder-volume"
 HEAT_SERVICES="heat-api heat-api-cfn heat-engine"
 CEILOMETER_SERVICES="ceilometer-agent-central ceilometer-agent-compute ceilometer-agent-notification ceilometer-alarm-evaluator ceilometer-alarm-notifier ceilometer-api ceilometer-collector"
