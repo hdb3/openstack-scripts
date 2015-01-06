0c37d0485e9d7d74322d6d325ff51403c97e661b
Nic Hart <nic@acer>
2015-01-06 11:27:03 +0000

    rh-init has to be all sudo...

--- rh-init.sh
+++ rh-init.sh
@@ -4,16 +4,16 @@ yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.
 yum install -y http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
 yum upgrade -y
 yum install -y `cat yum.list`
-sudo sed -i /etc/selinux/config -e 's/enforcing/disabled/'
-sudo systemctl disable firewalld
-sudo systemctl enable mariadb
-sudo systemctl enable openvswitch
-sudo systemctl enable rabbitmq-server
-sudo systemctl start mariadb
-sudo systemctl start openvswitch
-sudo systemctl start rabbitmq-server
-sudo rabbitmqctl change_password guest admin
+sed -i /etc/selinux/config -e 's/enforcing/disabled/'
+systemctl disable firewalld
+systemctl enable mariadb
+systemctl enable openvswitch
+systemctl enable rabbitmq-server
+systemctl start mariadb
+systemctl start openvswitch
+systemctl start rabbitmq-server
+rabbitmqctl change_password guest admin
 mysql_secure_installation
-sudo ln -s /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugin.ini
-sudo sed -i 's,plugins/openvswitch/ovs_neutron_plugin.ini,plugin.ini,g' /usr/lib/systemd/system/neutron-openvswitch-agent.service
-sudo chown -R apache:apache /usr/share/openstack-dashboard/static
+ln -s /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugin.ini
+sed -i 's,plugins/openvswitch/ovs_neutron_plugin.ini,plugin.ini,g' /usr/lib/systemd/system/neutron-openvswitch-agent.service
+chown -R apache:apache /usr/share/openstack-dashboard/static
