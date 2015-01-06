7d5c5c6f9b4d05b7e0acecf31da342f25f7ddd2e
Nic Hart <nic@acer>
2015-01-06 19:18:59 +0000

    a bit more picky about which packages to install

--- yum.sh
+++ yum.sh
@@ -2,4 +2,4 @@ yum install -y yum-plugin-priorities
 yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 yum install -y http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
 yum upgrade -y
-yum install -y `cat yum.list`
+yum install -y `grep -v '^#' yum.list`
