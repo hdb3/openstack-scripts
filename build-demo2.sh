98e33b877b91a4a6ab22d14c5407eaa9a6d91ba8
Nic Hart <nic@acer>
2015-01-07 00:45:20 +0000

    the new LU testbed config

---
+++ build-demo2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source openstack-utils.sh
+set -e
+
+	KADMIN		token-get
+	ADMINRC
+	NETIMAGE	"cirros"		"http://cdn.download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img"				qcow2	bare	True
+	NETIMAGE	"Fedora"	"https://download.fedoraproject.org/pub/alt/openstack/20/x86_64/Fedora-x86_64-20-20140618-sda.qcow2"	qcow2	bare    True
+	IMAGE-LIST
+
+	EXTNET		ext-net
+	SUBNET	ext-net		ext-subnet	10.30.65.1      10.30.65.0/24	10.30.65.2	10.30.65.50
+
+	NET		mgmt-net
+	SUBNET		mgmt-net	mgmt-subnet	172.16.0.1	172.16.0.0/24
+	ROUTER		mgmt-router
+	INTERFACE	mgmt-router	mgmt-subnet
+	GATEWAY		mgmt-router	ext-net
+
+	NET		server-net
+	SUBNET		server-net	server-subnet	172.16.1.1	172.16.1.0/24
+
+	NET		client-net
+	SUBNET		client-net	client-subnet	172.16.2.1	172.16.2.0/24
