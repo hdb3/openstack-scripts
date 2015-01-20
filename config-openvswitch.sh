1357807812b297b2af287f467e62583830677b6f
Nic Hart <nic@acer>
2015-01-20 12:52:53 +0000

    customising lm2 plugin.ini for multi interfaces

--- config-openvswitch.sh
+++ config-openvswitch.sh
@@ -3,8 +3,19 @@ n=1
 for interface in `echo $1 | tr ':' ' '`
 do
   echo "processing interface $n:$interface"
+  if [ $n -eq 1 ]
+  then
+    mappings="bridge_mappings = external1:br-ex1"
+    vlans="network_vlan_ranges = external1"
+  else
+    mappings="${mappings},external${n}:br-ex${n}"
+    vlans="${vlans},externa${n}1"
+  fi
   sudo ovs-vsctl --may-exist add-br br-ex${n}
   sudo ovs-vsctl --may-exist add-port br-ex${n} $interface
   sudo ip link set dev $interface up
   let n=n+1
 done
+echo "[ovs]"                        > os.neutron-bridge-mappings
+echo "${mappings}"                 >> os.neutron-bridge-mappings
+echo "${vlans}"                    >> os.neutron-bridge-mappings
