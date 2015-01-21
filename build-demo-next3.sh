6bf62e642dc90fe7430905a7bef3f23842b937cd
Nic Hart <nic@acer>
2015-01-21 18:37:36 +0000

    this is a working script for multiple interfaces on dell5 (Centos)

---
+++ build-demo-next3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+$( ./set-env.py )
+source openstack-utils.sh
+set -e
+	ADMINRC
+for n in 2 3 4
+  do
+	EXTNET		ext-net${n}		external${n}
+	SUBNET		ext-net${n}		ext-subnet${n}	10.30.65.1      10.30.65.0/24	10.30.65.1${n}0	10.30.65.1${n}9
+	NET		demo-net${n}
+	SUBNET		demo-net${n}	demo-subnet${n}	172.16.${n}.1	172.16.${n}.0/12
+	ROUTER		demo-router${n}
+	INTERFACE	demo-router${n}	demo-subnet${n}
+	GATEWAY		demo-router${n}	ext-net${n}
+	COMMAND		"sleep 3"
+  done
+	END
--- build-demo.sh
+++ build-demo.sh
@@ -3,24 +3,21 @@ $( ./set-env.py )
 source openstack-utils.sh
 set -e
 
-	KADMIN		token-get
 	ADMINRC
-	#COMMAND		"wget http://cdn.download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img"
 	#IMAGE		"cirros-0.3.3-x86_64"	cirros-0.3.3-x86_64-disk.img	qcow2	bare	True
 	#IMAGE		"Fedora-x86_64-20-20140618"	../Fedora-x86_64-20-20140618-sda.qcow2	qcow2	bare	True
 	NETIMAGE	"cirros"		"http://cdn.download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img"				qcow2	bare	True
 	NETIMAGE	"Fedora"	"https://download.fedoraproject.org/pub/alt/openstack/20/x86_64/Fedora-x86_64-20-20140618-sda.qcow2"	qcow2	bare    True
+	NETIMAGE	"ubuntu"	"https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img"	qcow2	bare    True
 	IMAGE-LIST
 
-	EXTNET		ext-net		external1
-	#		network name	subnet name	gateway		CIDR		alloc start	alloc end
-	SUBNET	ext-net		ext-subnet	10.30.65.1      10.30.65.0/24	10.30.65.2	10.30.65.50
-	# SUBNET	ext-net		ext-subnet	192.168.1.254      192.168.1.0/24	192.168.1.220	192.168.1.239
-	NET		demo-net
-	SUBNET		demo-net	demo-subnet	172.16.0.1	172.16.0.0/12
-	ROUTER		demo-router
-	INTERFACE	demo-router	demo-subnet
-	GATEWAY		demo-router	ext-net
+	EXTNET		ext-net1		external1
+	SUBNET	ext-net1		ext-subnet1	10.30.65.1      10.30.65.0/24	10.30.65.2	10.30.65.50
+	NET		demo-net1
+	SUBNET		demo-net1	demo-subnet1	172.16.0.1	172.16.0.0/12
+	ROUTER		demo-router1
+	INTERFACE	demo-router1	demo-subnet1
+	GATEWAY		demo-router1	ext-net1
 
 	COMMAND		"cinder		create	--display-name	demo-volume1	1"
 
