7ec5f73cf021bd2c800d4173b5a2efc064fc074c
Nic Hart <nic@x61>
2015-02-03 16:45:20 +0000

    restore epel install in  rh-init

--- rh-init.sh
+++ rh-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ev
 yum install -y yum-plugin-priorities
-#yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 yum install -y http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
 yum upgrade -y
 yum install -y `cat yum.list`
