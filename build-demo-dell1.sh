0334936e58a0bb2f5edd9df74bee7691195f22a6
Nic Hart <nic@x61>
2015-02-03 12:28:09 +0000

    typo in build-demo-dell1.sh

--- build-demo-dell1.sh
+++ build-demo-dell1.sh
@@ -19,7 +19,7 @@ set -e
 	SUBNET		ext-net2	ext-subnet2	192.168.0.1      192.168.0.0/24	192.168.0.100	192.168.0.199
 
 	NET		test-net
-	SUBNET		test-net	test-subnet	172.16.1.1	172.16.0.0/24
+	SUBNET		test-net	test-subnet	172.16.1.1	172.16.1.0/24
 	ROUTER		test-router
 	INTERFACE	test-router	test-subnet
 	GATEWAY		test-router	ext-net2
