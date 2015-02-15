86228ffbe36d1d3bd452f116748f89288004ff3f
Nicholas Hart <nic@lancs>
2015-02-15 18:14:40 +0000

    modify update-neutron-conf.sh so that it can be called multiple times

--- update-neutron-conf.sh
+++ update-neutron-conf.sh
@@ -4,5 +4,5 @@ source admin-openrc.sh
 SERVICE_TENANT_ID=`keystone tenant-list | awk '/ service / {print $2}'`
 echo "The SERVICE_TENANT_ID is $SERVICE_TENANT_ID"
 echo "updating $conf_file"
-sudo sed -i.bak -e "s/\$SERVICE_TENANT_ID/$SERVICE_TENANT_ID/" $conf_file
+sudo sed -i.bak -e "s/^nova_admin_tenant_id.*$/nova_admin_tenant_id = $SERVICE_TENANT_ID/" $conf_file
 sudo chown --reference ${conf_file}.bak $conf_file
