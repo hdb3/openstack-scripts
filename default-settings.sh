66e912bb4ec92906c7ebaf651bcaaf74a0b83210
Nic Hart <nic@lancs.uni>
2015-05-07 16:56:29 +0100

    WiP - adding vlan support

--- default-settings.sh
+++ default-settings.sh
@@ -1,4 +1,4 @@
 source ~/openstack-scripts/admin-openrc.sh
-nova keypair-add --pub_key x61.pub x61
+nova keypair-add --pub_key dell4.pem dell4
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 nova secgroup-add-rule default tcp 1 65535 0.0.0.0/0
