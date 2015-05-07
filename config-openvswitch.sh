6ccfe9c2beb1a14fa6a3cb8efacf12efcd2f3b04
Nic Hart <nic@lancs.uni>
2015-05-07 13:49:27 +0100

    updates for VLAN trunking external networks

--- config-openvswitch.sh
+++ config-openvswitch.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -e
-sudo ovs-vsctl --may-exist add-br br-ex1
-sudo ovs-vsctl --may-exist add-port br-ex1 $1
+#!bin/bash -e
+# sudo ovs-vsctl --may-exist add-br br-ex1
+# sudo ovs-vsctl --may-exist add-port br-ex1 $1
+# sudo ip link set dev $1 up
+sudo ovs-vsctl --may-exist add-br br-vlan
+sudo ovs-vsctl --may-exist add-port br-vlan $1 -- set port $1 vlan_mode=trunk trunk=100,101,200,201
 sudo ip link set dev $1 up
--- rh-init.sh
+++ rh-init.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -ev
 yum install -y yum-plugin-priorities
-yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum install -y epel-release
 yum install -y http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
 yum upgrade -y
 yum install -y `cat yum.list`
 for s in `grep -v '^#' rh-core-services` ; do systemctl enable $s ; systemctl start $s ; done
 sed -i /etc/selinux/config -e 's/enforcing/disabled/'
-systemctl disable NetworkManager
-systemctl stop NetworkManager
-systemctl disable firewalld
-systemctl stop firewalld
-rabbitmqctl change_password guest admin
-mysql_secure_installation
 ln -s /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugin.ini
 sed -i 's,plugins/openvswitch/ovs_neutron_plugin.ini,plugin.ini,g' /usr/lib/systemd/system/neutron-openvswitch-agent.service
 chown -R apache:apache /usr/share/openstack-dashboard/static
+systemctl disable NetworkManager firewalld
+systemctl stop NetworkManager firewalld
+rabbitmqctl change_password guest admin
+echo "please coose password 'root' when prompted (there is no existing password)"
+mysql_secure_installation
